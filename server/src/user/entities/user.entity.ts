3d6bd9bfbbc5824f6e52121a53af400eaf9b7db7
Skindetafenbolle <elited013@gmail.com>
2023-08-10 12:30:49 +0300

    created entity for category

--- server/src/user/entities/user.entity.ts
+++ server/src/user/entities/user.entity.ts
@@ -1,7 +1,9 @@
+import { Category } from 'src/category/entities/category.entity';
 import {
   Column,
   CreateDateColumn,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
@@ -16,6 +18,11 @@ export class User {
 
   @Column()
   password: string;
+
+  @OneToMany(() => Category, (category) => category.user, {
+    onDelete: 'CASCADE',
+  })
+  categories: Category[];
 
   @CreateDateColumn()
   createdAt: Date;
